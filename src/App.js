ba85ac260ece1b1808a25e8b9b52e2e47cc65de0
mamatha <mamathadeverakonda@gmail.com>
2024-04-09 23:09:50 +0530

    first solution

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import TopRatedPage from './components/TopRatedPage';
+import UpcomingMoviePage from './components/UpcomingMoviePage';
+import SingleMovieDetailPage from './components/SingleMovieDetailPage';
+import SearchedMoviePage from './components/SearchedMoviePage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/top-rated" element={<TopRatedPage/>} />
+          <Route path="/upcoming" element={<UpcomingMoviePage  />} />
+          <Route path="/movie/:movieId" element={<SingleMovieDetailPage />} /> 
+          <Route path="/search" element={<SearchedMoviePage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
